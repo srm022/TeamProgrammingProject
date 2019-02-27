8a47511881debcc3d32974ac521eda162cbcec84
Michał Surmacz <30442785+srm022@users.noreply.github.com>
2019-02-27 19:03:40 +0100

    Merge request test

--- PZProject/Program.cs
+++ PZProject/Program.cs
@@ -20,5 +20,6 @@ namespace PZProject
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
