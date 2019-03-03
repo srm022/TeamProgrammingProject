2cbbeb0dd369d14dd0a54fc200f7863e4b212e1e
Michał Surmacz <30442785+srm022@users.noreply.github.com>
2019-03-03 09:00:21 +0100

    Travis CI integration test

--- PZProject/Program.cs
+++ PZProject/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace PZProject
 {
@@ -20,6 +13,5 @@ namespace PZProject
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-
     }
 }
