88142d4b6971b6919e21d0280aebf132ad15c888
przemyslaw.p1213 <przemyslaw.p1213@gmail.com>
2019-04-14 14:44:24 +0200

    WIP Displaying user groups in log

--- PZProject/ClientApp/src/app/groups/groups.component.ts
+++ PZProject/ClientApp/src/app/groups/groups.component.ts
@@ -11,6 +11,11 @@ import { HttpHeaders} from '@angular/common/http';
 export class GroupsComponent implements OnInit {
 
   private zmienna: any;
+  private GroupNameArray = [];
+  private GroupIdArray = [];
+  private UserIdArray = [];
+  private i = 0;
+
   constructor(private http: HttpClient) {
     this.zmienna = localStorage.getItem('id_token');
 
@@ -19,9 +24,25 @@ export class GroupsComponent implements OnInit {
         'Authorization': 'Bearer ' + this.zmienna
       })
     }
-    console.log(this.zmienna);
+
     this.http.get('http://localhost:62333/groups', httpOptions).subscribe(result => {
       console.log(result);
+
+      while (result[this.i]) {
+        this.GroupIdArray.push(result[0]['groupId']);
+        this.GroupNameArray.push(result[0]['name']);
+        this.UserIdArray.push(result[0]['creatorId']);
+        this.GroupIdArray.push(result[1]['groupId']);
+        this.GroupNameArray.push(result[1]['name']);
+        this.UserIdArray.push(result[1]['creatorId']);
+        console.log(this.GroupIdArray[this.i]);
+        console.log(this.GroupNameArray[this.i]);
+        console.log(this.UserIdArray[this.i]);
+        this.i++;
+      }
+
+
+
     }, error => console.error(error));
   }
 
