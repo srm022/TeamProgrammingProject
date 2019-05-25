1817ebe98c67619bb1538cba1ee8eff04a7ccece
przemyslaw.p1213 <przemyslaw.p1213@gmail.com>
2019-05-25 15:03:00 +0200

    Created function to display user groups, code cleanup

--- PZProject/ClientApp/src/app/groups/groups.component.ts
+++ PZProject/ClientApp/src/app/groups/groups.component.ts
@@ -15,32 +15,36 @@ export class GroupsComponent implements OnInit {
   private iterator = 0;
 
   constructor(private http: HttpClient) {
-    this.token = localStorage.getItem('id_token');
-
-    let httpOptions = {
-      headers: new HttpHeaders({
-        'Authorization': 'Bearer ' + this.token
-      })
-    }
-
-    this.http.get('http://localhost:62333/groups', httpOptions).subscribe(result => {
-      console.log(result);
-
-      while (result[this.iterator]) {
-
-        this.UserGroupArray.push({
-          GroupId: result[this.iterator]['groupId'],
-          GroupName: result[this.iterator]['name'],
-          UserId: result[this.iterator]['creatorId']
-        });
-
-        this.iterator++;
-      }
-
-    }, error => console.error(error));
+ 
   }
 
   ngOnInit() {
+    this.displayUserGroups();
   }
 
+  displayUserGroups():void {
+  this.token = localStorage.getItem('id_token');
+
+  let httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': 'Bearer ' + this.token
+    })
+  }
+
+  this.http.get('http://localhost:62333/groups', httpOptions).subscribe(result => {
+    console.log(result);
+
+    while (result[this.iterator]) {
+
+      this.UserGroupArray.push({
+        GroupId: result[this.iterator]['groupId'],
+        GroupName: result[this.iterator]['name'],
+        UserId: result[this.iterator]['creatorId']
+      });
+
+      this.iterator++;
+    }
+
+  }, error => console.error(error));
+}
 }
