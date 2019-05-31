683e13047fff050b3b5ea348e9d9e3e442e6f8a0
przemyslaw.p1213 <przemyslaw.p1213@gmail.com>
2019-05-31 19:12:17 +0200

    User display WIP

--- PZProject/ClientApp/src/app/group-users-display/group-users-display.component.ts
+++ PZProject/ClientApp/src/app/group-users-display/group-users-display.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+import { Router } from '@angular/router';
+import { ToastsManager } from 'ng2-toastr';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-group-users-display',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupUsersDisplayComponent implements OnInit {
 
-  constructor() { }
+  token: any;
+  private iterator = 0;
+  private GroupNotesArray = [];
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private toastr: ToastsManager,
+    private vcr: ViewContainerRef) {
+      this.toastr.setRootViewContainerRef(vcr);
+    }
 
   ngOnInit() {
+    this.token = localStorage.getItem('id_token');
+  }
+
+  addUserGroupstoArray(result: Object | { [x: string]: any; }[]) {
+
+    while (result[this.iterator]) {
+
+      this.GroupNotesArray.push({
+        GroupId: result[this.iterator]['groupId'],
+        GroupName: result[this.iterator]['name'],
+        UserId: result[this.iterator]['creatorId'],
+        description: result[this.iterator]['description']
+      });
+
+      this.iterator++;
+    }
+  }
+  
+  displayGroupNotes() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + this.token
+      })
+    }
+    this.http.get('https://pzproject.azurewebsites.net/groups', httpOptions).subscribe(result => {
+      this.addUserGroupstoArray(result);
+      console.log(result);
+  
+    }, error => console.error(error));
+  
   }
 
 }
+
